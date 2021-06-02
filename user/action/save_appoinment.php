bc02e0f5a0f70cbc43352c0d16d64a721d092e20
Marvin Mosico <mqmosico@rex.com.ph>
2021-06-02 15:46:13 +0800

    check time of appointment

--- user/action/save_appoinment.php
+++ user/action/save_appoinment.php
@@ -5,19 +5,33 @@ if(isset($_POST['register'])){
   $client_name = $_POST['client_name'];
   $treatment_type = $_POST['treatment_type'];
   $date_of_appointment = date('Y-m-d', strtotime($_POST['date_of_appointment']));
-  $time_of_appointment = $_POST['time_of_appointment'];
+  $time_of_appointment = date('h:i:s', strtotime($_POST['time_of_appointment']));
   
-
-  $query = "INSERT INTO appointment(client_id,client_name, treatment_type, date_of_appointment, time_of_appointment,users_status) 
-  VALUES('$client_id','$client_name','$treatment_type', '$date_of_appointment', '$time_of_appointment',1)";
-
-  $result = mysqli_query($conn,$query);
-
-if ($result) {
-  echo "<script type=\"text/javascript\">". "alert('Save Success'); window.history.go(-1);" . "</script>";
-}
-else {
-    echo "<script type=\"text/javascript\">". "alert('Failed Save'); window.history.go(-1);" . "</script>";
-}
-}             
-?>  
+  //$checkrows = array();
+  $checkResult = array();
+  //$checkFinalResult = array();
+  //check time in appointment
+  $checkTime = mysqli_query($conn, "SELECT time_of_appointment as ta FROM appointment where appointment_status = ''");
+  while($checkrows = mysqli_fetch_assoc( $checkTime ) ) {
+    array_push( $checkResult, $checkrows );
+  }
+  // var_dump($checkResult);
+  // exit(1);
+  if (in_array($time_of_appointment,array_column($checkResult,'ta'))) {
+     echo "<script type=\"text/javascript\">". "alert('Time Already Exist'); window.history.go(-1);" . "</script>";
+     exit();
+  } else {
+    $query = "INSERT INTO appointment(client_id,client_name, treatment_type, date_of_appointment, time_of_appointment,users_status) 
+    VALUES('$client_id','$client_name','$treatment_type', '$date_of_appointment', '$time_of_appointment',1)";
+  
+    $result = mysqli_query($conn,$query);
+  
+  if ($result) {
+    echo "<script type=\"text/javascript\">". "alert('Save Success'); window.history.go(-1);" . "</script>";
+  }
+  else {
+      echo "<script type=\"text/javascript\">". "alert('Failed Save'); window.history.go(-1);" . "</script>";
+  }
+  }
+}       
+?>
